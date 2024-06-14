8665f2627a79dbcf8ef04810a897257188bc9412
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-15 00:06:35 +0530

    Update project6.scala

--- project6.scala
+++ project6.scala
@@ -2,11 +2,15 @@ object project6 {
 
   def main(args: Array[String]) : Unit = {
 
-    val radius = 5;
-    val pi = 3.14;
-    val volume = 4/3 * pi * radius * radius * radius;
-    println("volume of sphere: " + volume);
+    calculate();
+
 
   }
 
-}
+  def calculate(): Unit = {
+    val radius = 5;
+    val pi = 3.14;
+    val volume = 4 / 3 * pi * radius * radius * radius;
+    println("volume of sphere: " + volume);
+
+  }
