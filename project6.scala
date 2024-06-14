2947c98ce9883d9a86730ff1335a4df7076776ef
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-14 21:53:13 +0530

    volume of sphere

---
+++ project6.scala
@@ -0,0 +1,12 @@
+object project6 {
+
+  def main(args: Array[String]) : Unit = {
+
+    val radius = 5;
+    val pi = 3.14;
+    val volume = 4/3 * pi * radius * radius * radius;
+    println("volume of sphere: " + volume);
+
+  }
+
+}
