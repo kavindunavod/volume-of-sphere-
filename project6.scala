9327053be8e0af5f27b1d14f43e116b0873709f0
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-18 08:57:58 +0530

    Update project6.scala

--- project6.scala
+++ project6.scala
@@ -1,16 +1,10 @@
-object project6 {
+object sphereVolume{
+    def calculateVolume(radius: Double): Double={
+        val pi=math.Pi
+        (4.0/3.0)*pi*radius*radius*radius
+    }
 
-  def main(args: Array[String]) : Unit = {
-
-    calculate();
-
-
-  }
-
-  def calculate(): Unit = {
-    val radius = 5;
-    val pi = 3.14;
-    val volume = 4 / 3 * pi * radius * radius * radius;
-    println("volume of sphere: " + volume);
-
-  }
+    def main(args: Array[String]): Unit={
+        println(s"Volume of the sphere with radius 5: ${calculateVolume(5)}")
+    }
+}
